3bb5b7fc11ff58edeb09f546f1da0c315f01e714
Steven Calle <ztevencalle@gmail.com>
2024-10-27 23:46:13 -0500

    Se agrega funcion capturar productos

--- ventas.js
+++ ventas.js
@@ -20,3 +20,24 @@ function iniciarSesion() {
     alert("Demasiados intentos fallidos. El sistema se cerrará.");
     return false;
 }
+
+function capturarProductos() {
+    let totalProductos = 0;
+    let totalPrecio = 0;
+    let continuar = true;
+
+    while (continuar) {
+        let precio = prompt("Ingrese el precio del producto:");
+        precio = parseFloat(precio);
+
+        totalProductos += 1; // Incrementamos la cantidad de productos
+        totalPrecio += precio; // Sumamos el precio del producto al total
+
+        let agregarOtro = prompt("¿Desea agregar otro producto? (si/no):");
+        if (agregarOtro !== "si") {
+            continuar = false;
+        }
+    }
+
+    return { totalProductos: totalProductos, totalPrecio: totalPrecio };
+}
